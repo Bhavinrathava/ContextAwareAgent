4f95069576c4622d9057fb8725940851db65d1d2
Bhavinrathava <bhavinr9@gmail.com>
2023-12-22 20:32:36 -0600

    Code Cleanup

--- client.py
+++
@@ -1,5 +0,0 @@
-from langserve import RemoteRunnable
-
-remote_chain = RemoteRunnable("http://localhost:8000/category_chain/")
-print(remote_chain.invoke({"text": "colors"}))
-# >> ['red', 'blue', 'green', 'yellow', 'orange']
